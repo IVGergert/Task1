c1e84063c4701417b15ccb486e69eabfc1d97d07
Ilyagergert <Ilyagergert@gmail.com>
2025-11-25 20:08:24 +0300

    Сделал: поиск min\max значения массива определение суммы элементов массива.

--- src/main/java/com/gergert/task1/factory/StudentFactory.java
+++ src/main/java/com/gergert/task1/factory/StudentFactory.java
@@ -1,13 +1,13 @@
 package com.gergert.task1.factory;
 
 import com.gergert.task1.entity.Student;
-
-import java.util.logging.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class StudentFactory {
-    private static final Logger logger = Logger.getLogger(String.valueOf(StudentFactory.class));
+    private static final Logger logger = LogManager.getLogger(StudentFactory.class);
 
-    public static Student createStudent(int id, String name, int age) {
+    public Student createStudent(int id, String name, int age) {
         logger.info("Создание студента: " + name);
         return new Student(id, name, age);
     }
--- src/main/java/com/gergert/task1/service/impl/StudentServiceImpl.java
+++ src/main/java/com/gergert/task1/service/impl/StudentServiceImpl.java
@@ -1,31 +1,129 @@
 package com.gergert.task1.service.impl;
 
 import com.gergert.task1.entity.Student;
+import com.gergert.task1.exception.CustomException;
 import com.gergert.task1.service.StudentService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class StudentServiceImpl implements StudentService {
+public class StudentServiceImpl implements StudentService{
+
+    private static final Logger logger = LogManager.getLogger(StudentServiceImpl.class);
 
     @Override
-    public int findMaxAgeForStudent(Student[] Student) {
+    public int findMaxAgeForStudent(Student[] students) throws CustomException {
+        if (students == null || students.length == 0){
+            logger.error("Студенты не найдены");
+            throw new CustomException("Студенты не найдены");
+        }
 
-        return 0;
+        if (students[0] == null) {
+            logger.error("Первый элемент null");
+            throw new CustomException("Первый элемент null");
+        }
+
+        int maxAge = students[0].getAge();
+
+        for (Student student : students) {
+            if (student.getAge() > maxAge){
+                maxAge = student.getAge();
+            }
+        }
+
+        logger.info("Максимальный возраст студента найден: {}",  maxAge);
+        return maxAge;
     }
 
     @Override
-    public int findMinAgeForStudent(Student[] Student) {
+    public int findMinAgeForStudent(Student[] students) throws CustomException  {
+        if (students == null || students.length == 0){
+            logger.error("Студенты не найдены");
+            throw new CustomException("Студенты не найдены");
+        }
 
-        return 0;
+        if (students[0] == null) {
+            logger.error("Первый элемент null");
+            throw new CustomException("Первый элемент null");
+        }
+
+        int minAge = students[0].getAge();
+
+        for (Student student : students) {
+            if (student.getAge() < minAge){
+                minAge = student.getAge();
+            }
+        }
+
+        logger.info("Минимальный возраст студента найден: {}",  minAge);
+        return minAge;
     }
 
     @Override
-    public int findMaxStudentId(Student[] Student) {
+    public int findMaxStudentId(Student[] students) throws CustomException {
+        if (students == null || students.length == 0){
+            logger.error("Студенты не найдены");
+            throw new CustomException("Студенты не найдены");
+        }
 
-        return 0;
+        if (students[0] == null) {
+            logger.error("Первый элемент null");
+            throw new CustomException("Первый элемент null");
+        }
+
+        int maxStudentId = students[0].getStudentId();
+
+        for (Student student : students) {
+            if (student.getStudentId() > maxStudentId){
+                maxStudentId = student.getStudentId();
+            }
+        }
+
+        logger.info("Максимальный id студента найден: {}", maxStudentId);
+        return maxStudentId;
     }
 
     @Override
-    public int findMinStudentId(Student[] Student) {
+    public int findMinStudentId(Student[] students) throws CustomException {
+        if (students == null || students.length == 0){
+            logger.error("Студенты не найдены");
+            throw new CustomException("Студенты не найдены");
+        }
 
-        return 0;
+        if (students[0] == null) {
+            logger.error("Первый элемент null");
+            throw new CustomException("Первый элемент null");
+        }
+
+        int minStudentId = students[0].getStudentId();
+
+        for (Student student : students) {
+            if (student.getStudentId() < minStudentId){
+                minStudentId = student.getStudentId();
+            }
+        }
+
+        logger.info("Минимальный id студента найден: {}", minStudentId);
+        return minStudentId;
+    }
+
+    public int calculateTotalAge(Student[] students) throws CustomException{
+        if (students == null || students.length == 0){
+            logger.error("Студенты не найдены");
+            throw new CustomException("Студенты не найдены");
+        }
+
+        if (students[0] == null) {
+            logger.error("Первый элемент null");
+            throw new CustomException("Первый элемент null");
+        }
+
+        int calculateTotalAge = 0;
+
+        for (Student student : students) {
+            calculateTotalAge += student.getAge();
+        }
+
+        logger.info("Общий возраст студентов подсчитан: {}", calculateTotalAge);
+        return calculateTotalAge;
     }
 }
