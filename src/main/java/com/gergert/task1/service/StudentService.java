4f6309a4045420dc6f8d33414aaa226710d604be
Ilyagergert <Ilyagergert@gmail.com>
2025-11-26 20:28:29 +0300

    Сделал: поиск min\max значения массива определение суммы элементов массива.

--- src/main/java/com/gergert/task1/service/StudentService.java
+++ src/main/java/com/gergert/task1/service/StudentService.java
@@ -4,9 +4,9 @@ import com.gergert.task1.entity.Student;
 import com.gergert.task1.exception.CustomException;
 
 public interface StudentService {
-    int findMaxAgeForStudent(Student[] Student) throws CustomException;
-    int findMinAgeForStudent(Student[] Student) throws CustomException;
-    int findMaxStudentId(Student[] Student) throws CustomException;
-    int findMinStudentId(Student[] Student) throws CustomException;
+    int findMaxAgeForStudent(Student[] Students) throws CustomException;
+    int findMinAgeForStudent(Student[] Students) throws CustomException;
+    int findMaxStudentId(Student[] Students) throws CustomException;
+    int findMinStudentId(Student[] Students) throws CustomException;
     int calculateTotalAge(Student[] students) throws CustomException;
 }
