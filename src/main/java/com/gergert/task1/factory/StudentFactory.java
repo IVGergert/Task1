be41481aa9ae36bde1627055986b79040fe70f85
Ilyagergert <Ilyagergert@gmail.com>
2025-11-26 20:26:03 +0300

    Сделал: поиск min\max значения массива определение суммы элементов массива.

--- src/main/java/com/gergert/task1/factory/StudentFactory.java
+++ src/main/java/com/gergert/task1/factory/StudentFactory.java
@@ -1,17 +1,8 @@
 package com.gergert.task1.factory;
 
 import com.gergert.task1.entity.Student;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import com.gergert.task1.exception.CustomException;
 
-public class StudentFactory {
-    private static final Logger logger = LogManager.getLogger(StudentFactory.class);
-
-    public Student createStudent(int id, String name, int age) {
-        logger.info("Создание студента: " + name);
-        return new Student(id, name, age);
-    }
-
-
-
+public interface StudentFactory {
+    Student createStudent(int id, String name, int age) throws CustomException;
 }
